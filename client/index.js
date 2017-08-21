0778f27acce2dab5ea92d64a7f9310818ba46b55
Cillié Malan <cilliemalan@gmail.com>
2017-08-21 19:56:30 +0200

    added webpack and client files

---
+++ client/index.js
@@ -0,0 +1,10 @@
+
+function component() {
+    var element = document.createElement('div');
+
+    element.innerHTML = "Hello world!";
+
+    return element;
+}
+
+document.body.appendChild(component());
