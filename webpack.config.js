0f4bf3aff1c5853428c5c23eaf46b56e5f69caa5
Cillié Malan <cilliemalan@gmail.com>
2017-08-21 21:38:29 +0200

    Added html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './client/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'React from scratch'
+        })
+    ],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'www')
