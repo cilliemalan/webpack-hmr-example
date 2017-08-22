bb4b970ac212a4b83d59dd9c7646f1f9c2be9b53
Cillié Malan <cilliemalan@gmail.com>
2017-08-22 12:25:52 +0200

    Added webpack dev middleware

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const webpack = require('webpack');
+const webpackconfig = require('../webpack.config');
+const webpackMiddleware = require("webpack-dev-middleware");
 const app = express();
 
 app.use(express.static('www'));
+
+const wpmw = webpackMiddleware(webpack(webpackconfig),{});
+app.use(wpmw);
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
