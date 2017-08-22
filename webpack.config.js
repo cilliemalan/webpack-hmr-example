5ec51b72e053b0bf3ff81d3bfca81a590c1e31fb
Cillié Malan <cilliemalan@gmail.com>
2017-08-22 11:09:51 +0200

    Added devtool source maps to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './client/index.js',
+    devtool: 'inline-source-map',
     plugins: [
         new HtmlWebpackPlugin({
             title: 'React from scratch'
