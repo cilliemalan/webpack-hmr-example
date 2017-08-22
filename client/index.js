48f841ce9fe316b477580557256eb277023672ea
Cillié Malan <cilliemalan@gmail.com>
2017-08-22 15:18:22 +0200

    Added react image

--- client/index.js
+++ client/index.js
@@ -1,7 +1,13 @@
+import reactlogo from './content/react.svg';
 
 function component() {
     var element = document.createElement('div');
     element.innerHTML = "Hello world!";
+
+    var logo = new Image();
+    logo.src = reactlogo;
+    element.appendChild(logo);
+
     return element;
 }
 
