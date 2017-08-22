ace556f1528b6a649a372ca294f8cafa650ed730
Cillié Malan <cilliemalan@gmail.com>
2017-08-22 12:24:27 +0200

    Added webpack clean

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const dist = path.resolve(__dirname, 'www');
 
 module.exports = {
     entry: './client/index.js',
     devtool: 'inline-source-map',
     plugins: [
+        new CleanWebpackPlugin([dist]),
         new HtmlWebpackPlugin({
             title: 'React from scratch'
         })
     ],
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'www')
+        path: path.resolve(__dirname, dist)
     }
 };
