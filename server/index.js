b9c4849298f36787fa1f0f250b805d7bc6c1e72b
Cillié Malan <cilliemalan@gmail.com>
2017-08-22 22:25:26 +0200

    Serverside HMR via webpack-hot-middleware
    (cherry picked from commit 9776234718e86e3b4f0330a3e99c2081862b9165)

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const webpack = require('webpack');
 const webpackconfig = require('../webpack.config');
 const webpackMiddleware = require("webpack-dev-middleware");
+const webpackHotMiddleware = require("webpack-hot-middleware");
 const app = express();
 
 app.use(express.static('www'));
@@ -9,6 +10,9 @@ app.use(express.static('www'));
 const webpackCompiler = webpack(webpackconfig);
 const wpmw = webpackMiddleware(webpackCompiler,{});
 app.use(wpmw);
+
+const wphmw = webpackHotMiddleware(webpackCompiler);
+app.use(wphmw);
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
